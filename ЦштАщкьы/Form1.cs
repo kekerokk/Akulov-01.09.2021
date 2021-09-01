9a1af058f7d877c18f7da9612301b7a57818c8fc
Visual studio by Akulov <vspc_vs@VSPC34.RU>
2021-09-01 13:53:56 +0300

    Добавьте файлы проекта.

---
+++ ЦштАщкьы/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЦштАщкьы
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Double click");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("One click");
+            
+        }
+
+    }
+}
